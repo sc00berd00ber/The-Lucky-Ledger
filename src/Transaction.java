8a62732d43b3be7cb9c14fb45aa12b6997430874
Nat Tessema <natnaeltessema1@gmail.com>
2025-04-30 10:11:11 -0700

    Updated Transaction class

--- src/Transaction.java
+++ src/Transaction.java
@@ -16,24 +16,46 @@ public class Transaction {
         this.vendor = vendor;
         this.amount = amount;
     }
+
+//    //Parser
     public static Transaction fromCSV(String line) {
         String[] parts = line.split("\\|");
         return new Transaction(
-                LocalDate.parse(parts[0]),
-                LocalTime.parse(parts[1]),
-                parts[2],
-                parts[3],
-                Double.parseDouble(parts[4])
+                LocalDate.parse(parts[0].trim()),
+                LocalTime.parse(parts[1].trim()),
+                parts[2].trim(),
+                parts[3].trim(),
+                Double.parseDouble(parts[4].trim())
         );
     }
 
     public String toCSV() {
-        return String.format("%s|%s|%s|%s|%.2f", date, time, description, vendor, amount);
+        return String.format("%s | %s | %s | %s | %.2f", date, time, description, vendor, amount);
     }
     //Getters
     public LocalDate getDate() { return date; }
+    public LocalTime getTime() { return time; }
+    public String getDescription() { return description; }
     public double getAmount() { return amount; }
     public String getVendor() { return vendor; }
+
+    // Setters
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
 
     public String toString() {
         return String.format("%s | %s | %s | %s | %.2f", date, time, description, vendor, amount);
